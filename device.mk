bf367ce7053371894bf7024724630668f3022f26
TH779 <i@779.moe>
2021-08-24 13:04:10 +0800

    BGT_sprout: Import aidl QTI vibrator HAL from LA.UM.9.12.r1-11300-SMxx50.0
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I55828e7e445c4dc3d5382298dfec3c86d804bbd1
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -213,6 +213,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.nokia_BGT
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
