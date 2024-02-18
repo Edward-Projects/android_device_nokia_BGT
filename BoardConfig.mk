abac77cfbcf3d6f5361a35374a09209800320d3d
ReallySnow <reallysnow233@gmail.com>
2024-02-18 15:17:32 +0800

    BGT_sprout: vintf: Import Nokia change
    * From 00WW_3_49F
    * Switch to fqname
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,7 +123,11 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Vintf
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/nokia_vendor_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/manifest-generic.xml \
-    $(DEVICE_PATH)/configs/vintf/manifest-qva.xml
+    $(DEVICE_PATH)/configs/vintf/manifest-qva.xml \
+    $(DEVICE_PATH)/configs/vintf/nokia_manifest.xml
